d3a623dc8ff4f6bd62569310e2cd0307fba96840
Yvan Sanson <yvan.sanson@etu.umontpellier.fr>
2019-03-25 15:19:29 +0100

    Add Delete button on Travel table

--- Voyage Voyage/Controllers/TravelTVController.swift
+++ Voyage Voyage/Controllers/TravelTVController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class TravelTVController: NSObject, UITableViewDataSource, TravelVMDelegate {
+class TravelTVController: NSObject, UITableViewDataSource, UITableViewDelegate, TravelVMDelegate {
     
     var tableView: UITableView
     var travelVM : TravelVM?
@@ -36,6 +36,26 @@ class TravelTVController: NSObject, UITableViewDataSource, TravelVMDelegate {
         
     }
     
+    //MARK- UITableViewDelegate
+    /*func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let updateAction = UITableViewRowAction(style: .normal, title: "Edit", handler: {(action: UITableViewRowAction, indexPath: IndexPath)-> Void in})
+        let deleteAction = UITableViewRowAction(style: .destructive, title: "Delete", handler: {(action: UITableViewRowAction, indexPath: IndexPath)-> Void in
+            self.deletePrompt()
+        })
+        return [updateAction, deleteAction]
+    }*/
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .insert{
+            
+        }
+        
+        else if editingStyle == .delete{
+            self.deletePrompt()
+        }
+        
+    }
+    
     //MARK- TravelVMDelegate
     
     func dataSetChanged() {
@@ -52,5 +72,26 @@ class TravelTVController: NSObject, UITableViewDataSource, TravelVMDelegate {
     
     func travelAdded(at indexPath: IndexPath) {
         self.tableView.selectRow(at: indexPath, animated: true, scrollPosition: UITableView.ScrollPosition.middle)
+    }
+    
+    func deletePrompt(){
+        let dialogMessage = UIAlertController(title: "Confirm", message: "Are you sure you want to delete this?", preferredStyle: .alert)
+        
+        // Create OK button with action handler
+        let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
+            //deleteRecord()
+        })
+        
+        // Create Cancel button with action handlder
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel) { (action) -> Void in
+            return
+        }
+        
+        //Add OK and Cancel button to dialog message
+        dialogMessage.addAction(ok)
+        dialogMessage.addAction(cancel)
+        
+        // Present dialog message to user
+        //present(dialogMessage, animated: true, completion: nil)
     }
 }
