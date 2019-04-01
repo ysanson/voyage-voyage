985ac174a950dff3b394f0006d961c97d07b3fd8
Yvan Sanson <yvan.sanson@etu.umontpellier.fr>
2019-04-01 16:51:50 +0200

    On avance

---
+++ Voyage Voyage/Controllers/ExpenseDetails/ExpenseParticipantTVController.swift
@@ -0,0 +1,23 @@
+//
+//  ExpenseParticipantTVController.swift
+//  Voyage Voyage
+//
+//  Created by Yvan Sanson on 01/04/2019.
+//  Copyright © 2019 Yvan Sanson. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ExpenseParticipantTVController: NSObject, UITableViewDataSource, UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+    
+}
